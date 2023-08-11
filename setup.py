eec2f6b8c3422bffe27206c071b326f0b2734f66
Tim Berberich <tim-berberich@arcor.de>
2023-08-11 13:27:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 setup(
     # The name of your project that we discussed earlier.
     # This name will decide what users will type when they install your package.
-    # In my case it will be:
-    # pip install pydash-arnu515
     # This field is REQUIRED
     name="pySOFT",
 
@@ -19,22 +17,19 @@ setup(
     # eg: 1.0.0, 1.0.1, 3.0.2, 5.0-beta, etc.
     # You CANNOT upload two versions of your package with the same version number
     # This field is REQUIRED
-    version="0.0.1",
+    version="0.1.0",
 
     # The packages that constitute your project.
-    # For my project, I have only one - "pydash".
     # Either you could write the name of the package, or
     # alternatively use setuptools.findpackages()
-    #
     # If you only have one file, instead of a package,
     # you can instead use the py_modules field instead.
     # EITHER py_modules OR packages should be present.
     packages=find_packages(),
 
     # The description that will be shown on PyPI.
-    # Keep it short and concise
     # This field is OPTIONAL
-    description="python port of the C library SOFT 2.0, that computes fourier transforms on the rotation group SO(3).",
+    description="Python port of the C library SOFT 2.0, that computes fourier transforms on the rotation group SO(3).",
 
     # The content that will be shown on your project page.
     # In this case, we're displaying whatever is there in our README.md file
@@ -42,16 +37,13 @@ setup(
     long_description=README_MD,
 
     # Now, we'll tell PyPI what language our README file is in.
-    # In my case it is in Markdown, so I'll write "text/markdown"
-    # Some people use reStructuredText instead, so you should write "text/x-rst"
-    # If your README is just a text file, you have to write "text/plain"
     # This field is OPTIONAL
     long_description_content_type="text/markdown",
 
     # The url field should contain a link to a git repository, the project's website
     # or the project's documentation. I'll leave a link to this project's Github repository.
     # This field is OPTIONAL
-    #url="https://github.com/arnu515/pydash",
+    url="https://github.com/TimBerberich/pysoft",
 
     # The author name and email fields are self explanatory.
     # These fields are OPTIONAL
